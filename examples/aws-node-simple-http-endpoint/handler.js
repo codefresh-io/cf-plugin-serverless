8becc53b505198e76d40ab7587e1387197cf4b77
Alexei Ledenev <alexei.led@gmail.com>
2018-07-05 10:53:06 +0300

    improve

--- examples/aws-node-simple-http-endpoint/handler.js
+++ examples/aws-node-simple-http-endpoint/handler.js
@@ -4,7 +4,11 @@ module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+      message: `Hello, 
+      the current time: ${new Date().toTimeString()},
+      the event: ${JSON.stringify(event)}, 
+      the context: ${JSON.stringify(context)}
+      bye!`,
     }),
   };
 
